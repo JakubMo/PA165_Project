bcb38d4c10c8cbd3a010107f09b0e7070f7fb292
JakubMo <j.mozucha@gmail.com>
2015-10-27 00:39:00 +0100

    added DriveDao

--- WhiteTeamProject/src/main/java/cz/muni/fi/pa165/project/dao/DriveDao.java
+++ WhiteTeamProject/src/main/java/cz/muni/fi/pa165/project/dao/DriveDao.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.project.dao;
 
+import cz.muni.fi.pa165.project.entity.Drive;
+import java.util.List;
+
 /**
- *
- * @author jakub
+ * @author Jakub Mozucha | j.mozucha@gmail.com | created: 10/26/2015
  */
-public class DriveDao {
+
+public interface DriveDao {
+    
+    public List<Drive> getAllDrives();
+    
+    public Drive getDrive(Long id);
+    
+    public void updateDrive(Drive drive);
+    
+    public void deleteDrive(Drive drive);
+    
+    public void insertDrive(Drive drive);
     
 }
