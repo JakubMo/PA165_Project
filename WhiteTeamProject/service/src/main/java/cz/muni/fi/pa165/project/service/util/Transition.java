0bcda0773802caf196709fc5b40972c88f8dada3
JonisMarek <NeveS07@azet.sk>
2015-11-25 12:33:08 +0100

    Transition class hashCode, equals and toString

--- WhiteTeamProject/service/src/main/java/cz/muni/fi/pa165/project/service/util/Transition.java
+++ WhiteTeamProject/service/src/main/java/cz/muni/fi/pa165/project/service/util/Transition.java
@@ -1,5 +1,7 @@
 package cz.muni.fi.pa165.project.service.util;
 
+import java.util.Objects;
+
 /**
  * Defines transition from one state to another.
  *
@@ -45,5 +47,39 @@ public class Transition<T> {
      */
     public T getNewState() {
         return newState;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + oldState.hashCode();
+        hash = 37 * hash + newState.hashCode();
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Transition)) {
+            return false;
+        }
+        
+        final Transition<?> other = (Transition<?>) obj;
+        
+        return this.getOldState().equals(other.getOldState()) && 
+                this.getNewState().equals(other.getNewState());
+    }
+
+    @Override
+    public String toString() {
+        return "Transition{" + 
+                "oldState=" + oldState + 
+                ", newState=" + newState + 
+                '}';
     }
 }
