561dbce57299bae291a7253caa73e3f883771579
JonisMarek <NeveS07@azet.sk>
2015-12-16 22:46:27 +0100

    deleted some lines

--- WhiteTeamProject/web/src/main/java/cz/muni/fi/pa165/project/web/config/CarRentalSpringMvcConfig.java
+++ WhiteTeamProject/web/src/main/java/cz/muni/fi/pa165/project/web/config/CarRentalSpringMvcConfig.java
@@ -1,7 +1,6 @@
 package cz.muni.fi.pa165.project.web.config;
 
 import cz.muni.fi.pa165.project.data.CarRentalDataConfiguration;
-import cz.muni.fi.pa165.project.web.controllers.VehicleController;
 import javax.validation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,7 +26,6 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @Configuration
 @Import(CarRentalDataConfiguration.class)
-//@ComponentScan(basePackages = "cz.muni.fi.pa165.web.controllers")
 @ComponentScan(basePackages = {"cz.muni.fi.pa165.project.web.controllers"})
 public class CarRentalSpringMvcConfig extends WebMvcConfigurerAdapter {
     
