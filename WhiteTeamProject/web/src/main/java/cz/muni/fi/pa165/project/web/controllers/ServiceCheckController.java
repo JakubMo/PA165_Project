efb1355eb6cc7806b16ba4676dc8467797df5b30
tborcin <tom.borcin@gmail.com>
2015-12-25 23:33:21 +0100

    new ServiceCheck fix

--- WhiteTeamProject/web/src/main/java/cz/muni/fi/pa165/project/web/controllers/ServiceCheckController.java
+++ WhiteTeamProject/web/src/main/java/cz/muni/fi/pa165/project/web/controllers/ServiceCheckController.java
@@ -8,23 +8,29 @@ import cz.muni.fi.pa165.project.enums.DriveStatus;
 import cz.muni.fi.pa165.project.enums.ServiceCheckStatus;
 import cz.muni.fi.pa165.project.facade.ServiceCheckFacade;
 import cz.muni.fi.pa165.project.facade.VehicleFacade;
+import cz.muni.fi.pa165.project.util.DataAccessExceptionImpl;
+import cz.muni.fi.pa165.project.web.utils.EmployeeDTOEditor;
+import cz.muni.fi.pa165.project.web.utils.VehicleDTOEditor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.inject.Inject;
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -44,52 +50,95 @@ public class ServiceCheckController {
 	@Autowired
 	private VehicleFacade vehicleFacade;
 
+	@Inject
+	private VehicleDTOEditor vehicleDTOEditor;
+
+	@InitBinder
+	public void InitBinder(WebDataBinder binder) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		simpleDateFormat.setLenient(true);
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(simpleDateFormat, true));
+		binder.registerCustomEditor(VehicleDTO.class, vehicleDTOEditor);
+	}
+
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String list(Model model) {
-		model.addAttribute("services", serviceCheckFacade.getAll());
-		log.info("Model: " + model);
+	public String list(Model model, RedirectAttributes redirectAttributes) {
+		log.info("Listing service checks");
+		try {
+			model.addAttribute("services", serviceCheckFacade.getAll());
+		} catch (Exception e) {
+			log.trace(e.getMessage());
+			redirectAttributes.addFlashAttribute("alert_danger", e.getLocalizedMessage());
+			return "redirect:/";
+		}
 		return "servicecheck/list";
 	}
 
 	@RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
-	public String detail(@PathVariable long id, Model model) {
-		model.addAttribute("serviceCheck", serviceCheckFacade.get(id));
+	public String detail(@PathVariable long id, Model model, RedirectAttributes redirectAttributes) {
+		log.info("Detail of service check with id: " + id);
+		try {
+			model.addAttribute("serviceCheck", serviceCheckFacade.get(id));
+		} catch (Exception e) {
+			log.trace(e.getMessage());
+			redirectAttributes.addFlashAttribute("alert_danger", e.getLocalizedMessage());
+			return "redirect:/servicecheck/list";
+		}
 		return "servicecheck/detail";
 	}
 
 	@RequestMapping(value = "/new", method = RequestMethod.GET)
-	public String newVehicle(Model model) {
-		model.addAttribute("statuses", ServiceCheckStatus.values());
-		model.addAttribute("vehicles", vehicleFacade.getAll());
-		model.addAttribute("servicecheckCreate", new ServiceCheckCreateDTO());
+	public String newVehicle(Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriComponentsBuilder) {
+		log.info("New service check");
+		try {
+			model.addAttribute("statuses", ServiceCheckStatus.values());
+			model.addAttribute("vehicles", vehicleFacade.getAll());
+			model.addAttribute("servicecheckCreate", new ServiceCheckCreateDTO());
+		} catch (Exception e) {
+			log.trace(e.getMessage());
+			redirectAttributes.addFlashAttribute("alert_danger", e.getLocalizedMessage());
+			return "redirect:" + uriComponentsBuilder.path("/servicecheck/new").toUriString();
+		}
 		return "servicecheck/new";
 	}
 
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String create(@Valid @ModelAttribute("serviceCheckCreate") ServiceCheckCreateDTO serviceCheckCreateDTO, BindingResult bindingResult,
 						 Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriComponentsBuilder) {
-		if (!bindingResult.hasErrors()) {
-			serviceCheckFacade.createServiceCheck(serviceCheckCreateDTO);
-			redirectAttributes.addFlashAttribute("alert_success", "Service check was created.");
-		} else {
-			bindingResult.getGlobalErrors().stream().forEach((ObjectError oe) -> {
-				log.trace("ObjectError: {}", oe);
-			});
+		log.info("Creating service check");
+		try{
+			if(!bindingResult.hasErrors()) {
+				serviceCheckFacade.createServiceCheck(serviceCheckCreateDTO);
+				redirectAttributes.addFlashAttribute("alert_success", "Service check was created");
+			} else {
+				bindingResult.getGlobalErrors().stream().forEach((ObjectError oe) -> {
+					log.trace("ObjectError: {}", oe);
+				});
 
-			bindingResult.getFieldErrors().stream().forEach((FieldError fe) -> {
-				log.trace("FieldError: {}", fe);
-				model.addAttribute(fe.getField() + "_error", fe.getDefaultMessage());
-			});
-			return "servicecheck/new";
+				bindingResult.getFieldErrors().stream().forEach((FieldError fe) -> {
+					log.trace("FieldError: {}", fe);
+					redirectAttributes.addFlashAttribute("alert_danger", "invalid " + fe.getField());
+				});
+				return "redirect:" + uriComponentsBuilder.path("/servicecheck/new").toUriString();
+			}
+		} catch (Exception e) {
+			log.trace(e.getMessage());
+			redirectAttributes.addFlashAttribute("alert_danger", e.getLocalizedMessage());
+			return "redirect:" + uriComponentsBuilder.path("/servicecheck/new").toUriString();
 		}
-
 		return "redirect:" + uriComponentsBuilder.path("/servicecheck/list").toUriString();
 	}
 
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
 	public String delete(@PathVariable long id, Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriComponentsBuilder) {
-		serviceCheckFacade.deleteServiceCheck(id);
-		redirectAttributes.addFlashAttribute("alert_success", "Service check with ID " + id + " was deleted.");
-		return "redirect:" + uriComponentsBuilder.path("/servicecheck/list").toUriString();
+		log.info("Deleting service check with id: " + id);
+		try {
+			serviceCheckFacade.deleteServiceCheck(id);
+			redirectAttributes.addFlashAttribute("alert_success", "Service check with ID " + id + " was deleted.");
+		} catch (Exception e) {
+			log.trace(e.getMessage());
+			redirectAttributes.addFlashAttribute("alert_danger", e.getLocalizedMessage());
+		}
+		return "redirect:/servicecheck/list";
 	}
 }
--- WhiteTeamProject/web/src/main/webapp/WEB-INF/jsp/servicecheck/new.jsp
+++ WhiteTeamProject/web/src/main/webapp/WEB-INF/jsp/servicecheck/new.jsp
@@ -6,56 +6,72 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
-<%@taglib tagdir="/WEB-INF/tags" prefix="my"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@taglib tagdir="/WEB-INF/tags" prefix="my" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
 
 <my:layout title="Create service check">
     <jsp:attribute name="body">
         <div class="row">
-					<form:form method="post" action="${pageContext.request.contextPath}/servicecheck/create" modelAttribute="servicecheckCreate" cssClass="col-md-5">
+					<form:form method="post" action="${pageContext.request.contextPath}/servicecheck/create"
+										 modelAttribute="servicecheckCreate" cssClass="col-md-5">
 
 						<div class="form-group">
 							<form:label path="status">State:</form:label>
 							<div>
-								<form:select path="status" items="${statuses}" />
+								<form:select path="status" items="${statuses}"/>
 							</div>
 							<c:if test="${not empty status_error}">
-								<p style="color: red"><c:out value="${status_error}" /></p>
+								<p style="color: red"><c:out value="${status_error}"/></p>
 							</c:if>
 						</div>
 
 						<div class="form-group">
 							<form:label path="serviceCheckDate">Date:</form:label>
 							<div>
-								<form:input path="serviceCheckDate" cssClass="form-control" placeholder="Enter date" type="date" />
+								<form:input path="serviceCheckDate" cssClass="form-control" placeholder="Enter date" type="date"/>
 							</div>
 							<c:if test="${not empty serviceCheckDate_error}">
-								<p style="color: red"><c:out value="${serviceCheckDate_error}" /></p>
+								<p style="color: red"><c:out value="${serviceCheckDate_error}"/></p>
 							</c:if>
 						</div>
 
 						<div class="form-group">
-							<form:label path="vehicle">Vehicle VIN:</form:label>
+							<form:label path="vehicle" cssClass="control-label">Vehicle</form:label>
 							<div>
-								<form:select path="vehicle" items="${vehicles}" />
+								<form:select path="vehicle">
+									<c:forEach items="${vehicles}" var="vehicle">
+										<form:option
+												value="${vehicle.id}">${vehicle.brand} ${vehicle.model} ${vehicle.yearOfProduction}, vin: ${vehicle.vin},
+											mileage: ${vehicle.mileage}</form:option>
+									</c:forEach>
+								</form:select>
 							</div>
-							<c:if test="${not empty vehicle_error}">
-								<p style="color: red"><c:out value="${vehicle_error}" /></p>
-							</c:if>
 						</div>
 
 						<div class="form-group">
 							<form:label path="serviceEmployee">Employee:</form:label>
 							<div>
-								<form:input path="serviceEmployee" cssClass="form-control" placeholder="Enter employee name" />
+								<form:input path="serviceEmployee" cssClass="form-control" placeholder="Enter employee name"/>
 							</div>
 							<c:if test="${not empty serviceEmployee_error}">
-								<p style="color: red"><c:out value="${serviceEmployee_error}" /></p>
+								<p style="color: red"><c:out value="${serviceEmployee_error}"/></p>
 							</c:if>
 						</div>
 
-						<button type="submit" class="btn btn-default">Submit</button>
+						<div class="form-group">
+							<form:label path="report">Report:</form:label>
+							<div>
+								<form:input path="report" cssClass="form-control" placeholder="Enter report"/>
+							</div>
+							<c:if test="${not empty report_error}">
+								<p style="color: red"><c:out value="${report_error}"/></p>
+							</c:if>
+						</div>
+
+						<a href="${pageContext.request.contextPath}/servicecheck/list" class="btn btn-default">Back</a>
+						<button type="submit" class="btn btn-primary">Create</button>
 					</form:form>
 				</div>
     </jsp:attribute>
